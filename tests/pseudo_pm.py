275d7ef47474342ce97a638c082731738068d960
Ari Gaskins <arigaskins.tech@gmail.com>
2021-10-09 16:32:51 -0400

    file management

---
+++ tests/pseudo_pm.py
@@ -0,0 +1,19 @@
+# import fernet module from cryptography 
+# import sql.connector to establish connection 
+
+# need function that outputs menu for what user wants to do
+# also needs to ask for master password
+
+# need class for data objects that collect user data to be uploaded to db
+# values of object are username, password, website, email and backup email 
+
+# need function that takes key and password to encrypt password
+# return encrypted password
+
+# need to have a copy to clipboard function 
+
+# need function that can store encrypted passwords into database 
+# will also store websites, usernames, emails, and backup emails (optional)
+
+
+# NOTE: organize into model, view, controller format

da1e78bc78213d97a29ebb8c7c2b8317c23b61b5
Ari Gaskins <arigaskins.tech@gmail.com>
2022-05-18 16:21:00 -0400

    test cryptography library encryption and decryption

--- tests/cryptography_test.py
+++ tests/cryptography_test.py
@@ -1,12 +1,29 @@
-# import required module from cryptography library
 import os
 from cryptography.fernet import Fernet
+
+# encode string to bytes
+plaintext = 'password'.encode()
 
 # key generation 
 key = Fernet.generate_key()
 
+# create fernet object
+f = Fernet(key)
+
+# encrypt plaintext using key
+encrypted = f.encrypt(plaintext)
+print(encrypted)
+
+# decrypt encrypted text
+decrypted = f.decrypt(encrypted)
+print(decrypted)
+
+
+
+
+
 # string the key in a file
-with open('filekey.key', 'x') as filekey:
-    filekey.write(key)
+#with open('filekey.key', 'x') as filekey:
+#    filekey.write(key)
                 
 
